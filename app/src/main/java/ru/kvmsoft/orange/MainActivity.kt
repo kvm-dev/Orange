1f49bda4fa57ad7a735f1cf49bc85653242d4ff0
pr0r0ck <mastadji@gmail.com>
2023-09-15 18:34:44 +0300

    button

--- app/src/main/java/ru/kvmsoft/orange/MainActivity.kt
+++ app/src/main/java/ru/kvmsoft/orange/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tv:TextView = findViewById(R.id.tv)
+        val tv:Button = findViewById(R.id.tv)
         tv.setOnClickListener {
             viewModel.makeRequest()
         }
