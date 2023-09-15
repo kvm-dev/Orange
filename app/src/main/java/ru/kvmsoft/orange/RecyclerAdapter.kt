1e386910e31bfda7141b8ec2787d22761df735ab
ant <theant@mail.ru>
2023-09-15 23:23:38 +0400

    рейт новости +список компаний на которые она повлияет

--- app/src/main/java/ru/kvmsoft/orange/RecyclerAdapter.kt
+++ app/src/main/java/ru/kvmsoft/orange/RecyclerAdapter.kt
@@ -3,10 +3,13 @@ package ru.kvmsoft.orange
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter(val internalData:Last5NewsResponse) : RecyclerView.Adapter<RecyclerAdapter.NewsHolder>() {
+class RecyclerAdapter(val internalData: Last5NewsResponse) :
+    RecyclerView.Adapter<RecyclerAdapter.NewsHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
@@ -16,13 +19,21 @@ class RecyclerAdapter(val internalData:Last5NewsResponse) : RecyclerView.Adapter
 
     override fun onBindViewHolder(holder: NewsHolder, position: Int) {
         val news = internalData[position]
-        holder.newsName.text = news.text
+        holder.newsRate.text = news.rate.toString()
+        for (i in 0..news.companiesAffected.lastIndex) {
+
+
+            holder.companName.text = news.companiesAffected[i].toString()
+            holder.affectedCompanies.addView(holder.companName, i)
+        }
     }
 
     override fun getItemCount(): Int = internalData.size
 
     class NewsHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val newsName: TextView = view.findViewById(R.id.item_news_text)
-
+        val newsRate: TextView = view.findViewById(R.id.item_news_rate)
+        val affectedCompanies: LinearLayoutCompat = view.findViewById(R.id.affected_company)
+        val viewAffected = LayoutInflater.from(view.context).inflate(R.layout.item_affected, affectedCompanies, false)
+        val companName = viewAffected.findViewById<TextView>(R.id.affected_company)
     }
 }
