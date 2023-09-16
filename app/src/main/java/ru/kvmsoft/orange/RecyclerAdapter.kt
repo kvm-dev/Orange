cfae35873187d9d92640e43999494b2a12fee47e
ant <theant@mail.ru>
2023-09-16 11:26:19 +0400

    убрал список. просто текст

--- app/src/main/java/ru/kvmsoft/orange/RecyclerAdapter.kt
+++ app/src/main/java/ru/kvmsoft/orange/RecyclerAdapter.kt
@@ -3,9 +3,7 @@ package ru.kvmsoft.orange
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerAdapter(val internalData: Last5NewsResponse) :
@@ -20,20 +18,13 @@ class RecyclerAdapter(val internalData: Last5NewsResponse) :
     override fun onBindViewHolder(holder: NewsHolder, position: Int) {
         val news = internalData[position]
         holder.newsRate.text = news.rate.toString()
-        for (i in 0..news.companiesAffected.lastIndex) {
-
-
-            holder.companName.text = news.companiesAffected[i].toString()
-            holder.affectedCompanies.addView(holder.companName, i)
-        }
+        holder.newsCompany.text = news.companiesAffected.toString()
     }
 
     override fun getItemCount(): Int = internalData.size
 
     class NewsHolder(view: View) : RecyclerView.ViewHolder(view) {
         val newsRate: TextView = view.findViewById(R.id.item_news_rate)
-        val affectedCompanies: LinearLayoutCompat = view.findViewById(R.id.affected_company)
-        val viewAffected = LayoutInflater.from(view.context).inflate(R.layout.item_affected, affectedCompanies, false)
-        val companName = viewAffected.findViewById<TextView>(R.id.affected_company)
+        val newsCompany: TextView = view.findViewById(R.id.item_news_comp)
     }
 }
